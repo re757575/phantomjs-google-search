317dff5f0472c25e567d940f9ab9b0ba86a6821a
Alex.Dai <re757575@gmail.com>
2015-10-03 20:21:19 +0800

    Modify: use waitFor replace of setTimeout

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(1);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 250); //< repeat check every 250ms
+};
+
 var page = require('webpage').create();
 var url = 'http://www.google.com/';
 
@@ -37,18 +60,22 @@ page.open(url, function(status) {
 
             console.log('wait render...');
 
-            setTimeout(function () {
+            waitFor(function() {
+              // Check in the page if a specific element is now visible
+              return page.evaluate(function() {
+                return $('.srg').length > 0;
+              });
+            }, function() {
               page.render('google-search.png');
               console.log('render done');
               phantom.exit(0);
-            },5000);
+            });
 
             // page.onLoadFinished = function() {
             //   page.render('google-search.jpeg', {format: 'jpeg', quality: '100'});
             //   console.log('render done');
             //   phantom.exit(0);
             // };
-
         });
     }
 });
